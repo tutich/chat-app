53a39b9e102ed03454b636292f2c773abc2e6b36
tutich <pkoinange02@gmail.com>
2024-01-11 23:58:46 +0300

    project jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Impressive!Though it seems the drag feature could be improved. But overall it looks incredible. You've 
+      nailed the design and the responsiveness at various breakpoints works really well.
+
+      Woah your project look awesome! How long have you been coding for? I'm still new, but think I want to dive 
+      into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!
+
+      If you're still new i would reccomend focusing on fundamentals of HTML, CSS and JS before considering React.
+      It's very tempting to jump ahead but lay a solid foundation first.
+
+      I couldn't agree more with this.Everything moves so fast and it always seems like everyone knows the newest 
+      library/framework.But the fundamentals are what they stay constant.
     </div>
   );
 }
